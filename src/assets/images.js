de9cc17c370edc97e69cb3f71e6d317e0e218ebd
Ghifani <ghifaniikhsan114@gmail.com>
2024-04-18 21:34:49 +0700

    Menambahkan gambar peace, blackLivesMatter, palestine di file iamges

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,3 +1,6 @@
 export const logo = '../../assets/images/logo.png';
 export const ebook = '../../assets/images/ebook.jpg';
 export const medal = '../../assets//images/medal.png';
+export const blackLivesMatter = '../../assets//images/blackLivesMatter.jpg';
+export const peace = '../../assets/images/peace.jpg';
+export const palestine = '../../assets/images/peace.jpg';
