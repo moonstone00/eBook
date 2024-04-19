297647d4b5a9bf631f952d2770226119b45999c0
Ghifani <ghifaniikhsan114@gmail.com>
2024-04-19 14:05:53 +0700

    Membuat footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
-import {Instagram, X, LinkedIn} from '@mui/icons-material';
+import {Instagram, X, LinkedIn, Link} from '@mui/icons-material';
+import { CopyRight, EmailText } from '../../variables/Text';
+import { logo } from '../../assets/images';
 
 export default function Footer() {
     return (
-        <footer className='min-h-[50vh]'>
-            This is footer
+        <footer className='min-h-[50vh] p-4'>
+            <div className='text-xs cursive text-slate-600 py-12'>
+                <EmailText/>
+            </div>
+            <div className='flex justify-between items-center border-t border-slate-200 pt-12'>
+                <div className='w-12 h-12'>
+                    <img src={logo} alt="This is logo" />
+                </div>
+                <div className='flex gap-2'>
+                    <Instagram 
+                        sx={{ color: '#716e6d' }}
+                    />
+                    <X
+                        sx={{ color: '#716e6d' }}
+                    />
+                    <LinkedIn
+                        sx={{ color: '#716e6d' }}
+                    />
+                </div>
+            </div>
+            <div className='text-end text-[#716e6d] text-xs mt-6'>
+                <CopyRight/>
+            </div>
         </footer>
     )
 }
